e65c71882513c334a58d6b29861a32434c4feade
klishtshashank <100855002+klishtshashank@users.noreply.github.com>
2022-03-13 15:24:11 +0530

    Time: 7 ms (28.81%), Space: 8.8 MB (83.91%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -10,10 +10,12 @@ public:
                       
         for(int i=0;i<candies.size();i++)
         {
-            if(candies[i]+ extraCandies >= maxi)
-                ans.push_back(true);
-            else
-                ans.push_back(false);
+            // if(candies[i]+ extraCandies >= maxi)
+            //     ans.push_back(true);
+            // else
+            //     ans.push_back(false);
+(candies[i]+ extraCandies >= maxi )?ans.push_back(true):ans.push_back(false);
+            
         }
                       return ans;
     }
