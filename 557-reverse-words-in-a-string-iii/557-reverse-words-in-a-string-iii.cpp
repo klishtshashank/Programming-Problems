0dc2f327b79923d402b3802be54fbe77a9364884
SHASHANK SHEKHER <100855002+klishtshashank@users.noreply.github.com>
2022-06-06 00:27:58 +0530

    Time: 16 ms (68.37%), Space: 9.7 MB (75.76%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -4,11 +4,11 @@ public:
         int i=0;
         for(int j=0;j<s.size();j++){
             if(s[j]==' '){
-                reverse(s.begin()+i, s.begin()+j);//when find a space just reverse the word;
-                i=j+1;//update j pointer for next reversal
+                reverse(s.begin()+i, s.begin()+j);
+                i=j+1;
             }
         }
-        reverse(s.begin()+i,s.end());//explicitely reverse the last word,as the last word don't has ' '(space) in the end;
+        reverse(s.begin()+i,s.end());
         return s;
     }
 };
