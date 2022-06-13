be12c870b423ca844e44660190d70b9e7002cdb7
SHASHANK SHEKHER <100855002+klishtshashank@users.noreply.github.com>
2022-06-13 10:32:04 +0530

    Time: 10 ms (45.95%), Space: 10.4 MB (40.31%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-         sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        vector<int> result;
-        int i=0,j=0;
-        while(i<nums1.size()&&j<nums2.size()){
-            if(nums1[i]>nums2[j]){
-                j++;
-            }
-            else if(nums2[j]>nums1[i]){
-                i++;
-            }
-            else{
-                if(!count(result.begin(),result.end(),nums1[i]))
-                    result.push_back(nums1[i]);
-                i++;
-                j++;
-            }
+         
+unordered_map<int, int> count;
+        int n= nums1.size();
+        int m= nums2.size();
+    for(int i=0;i<n;i++)
+        {
+            count[nums1[i]]++;
         }
-       return result; 
-//         unordered_map<int, int> m;
-//        for(int i=0;i<nums1.size();i++)
-//        {
-//            m[nums1[i]]++;
-//        }
-//          for(int i=0;i<nums1.size();i++)
-//        {
-            
-//            if(m[nums1[i]]>1)
-//                m[nums1[i]]=1;
-//        }
-//         for(int j=0;j<nums2.size();j++)
-//         {
-//             m[nums2[j]]++;
-//         }
-//         vector<int> ans;
-//         for(int i=0;i<nums1.size();i++)
-//         {
-//             if(m[nums1[i]]>1)
-//                 ans.push_back(nums1[i]);
-//         }
-//         return ans;
+        vector<int> ans;
+  for(int i=0;i<m;i++)
+  {  
+      if(count[nums2[i]]>0)
+      {
+          ans.push_back(nums2[i]);
+          count[nums2[i]]=0;
+      }
+  }
+      return ans;
     }
 };
