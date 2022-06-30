3ff2cf2929fb42ad9ab0b79dc63af07aeb38a99d
SHASHANK SHEKHER <100855002+klishtshashank@users.noreply.github.com>
2022-06-30 11:25:05 +0530

    Time: 27 ms (9.46%), Space: 11 MB (15.75%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -2,21 +2,41 @@ class Solution {
 public:
     int minMoves2(vector<int>& nums) {
         int n=nums.size();
-        int x;
-        sort(nums.begin(), nums.end());
-        if(n%2==0)
-        {
-         x= (nums[n/2]+ nums[(n/2)-1])/2;
-        }
-        else 
-            x= nums[n/2];
-        
+              sort(nums.begin(), nums.end());
         int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            ans= ans+ abs(nums[i]-x);
-        }
-        
+for(int i=0;i<n/2;i++)
+{
+    ans += nums[n-i-1]- nums[i];
+}
         return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        int x;
+     // sort(nums.begin(), nums.end());
+//         int n=nums.size();
+//         int x;
+//         sort(nums.begin(), nums.end());
+//         if(n%2==0)
+//         {
+//          x= (nums[n/2]+ nums[(n/2)-1])/2;
+//         }
+//         else 
+//             x= nums[n/2];
+        
+//         int ans=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             ans= ans+ abs(nums[i]-x);
+//         }
+        
+//         return ans;
     }
 };
