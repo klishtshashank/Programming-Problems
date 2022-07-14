74b37e10d9e528ebfbe29e6937b91fc1905bb923
SHASHANK SHEKHER <100855002+klishtshashank@users.noreply.github.com>
2022-07-14 15:59:28 +0530

    Time: 29 ms (74.35%), Space: 15.4 MB (14.71%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -8,38 +8,30 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-//       if(head==NULL)
-//           return NULL;
-//         ListNode* small= removeElements(head-> next, val);
-//         if(head->val== val)
-//         {
-//             ListNode* temp= head;
-//             head= head-> next;
-//             delete(temp);
-//         }
-//         else{
-//             if(small!=NULL)
-//             head-> next= small;
-//             else if(small==NULL && head-> val== val)
-//                 return NULL;
-//         }
-        
-//         return head;
+     
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        if(head==NULL)
-            return head;
+            if(head==NULL)
+                return head;
         
-        head->next= removeElements(head->next, val);
         
-       return  (head->val==val)? head->next: head;
+            head->next= removeElements(head->next,val);
         
+        return head->val==val ? head->next:head;
+        //it is not working but upper wala is coreect
+     /*   if(head==NULL)
+          return head;
+        ListNode* small= removeElements(head-> next, val);
+        if(head->val== val)
+        {
+            return head-> next;
+        }
+        
+        
+        return head;
+        */
 
     }
 };
-  // ListNode* removeElements(ListNode* head, int T) {
-  //       if(!head) return nullptr;
-  //       head -> next = removeElements(head -> next, T);
-  //       return head -> val == T ? head -> next : head;
-  //   }
+  
