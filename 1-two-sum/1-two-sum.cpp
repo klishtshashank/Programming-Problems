e96dfc1206619a4acf7a30684a95bd84acc14c9e
SHASHANK SHEKHER <100855002+klishtshashank@users.noreply.github.com>
2022-09-21 22:47:58 +0530

    Time: 808 ms (16.46%), Space: 10.2 MB (74.56%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -5,9 +5,7 @@ public:
          for(int i=0;i<nums.size();i++){
              for(int j= i+1;j<nums.size();j++){
                  if(nums[j]+nums[i]==target){
-                     ans.push_back(i);
-                     ans.push_back(j);
-                     break;
+                        return {i,j};
                     
                  }
              }
