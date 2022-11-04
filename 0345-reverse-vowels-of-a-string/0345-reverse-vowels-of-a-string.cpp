919e07ac6215b9251a043e00f9af261c09047994
SHASHANK SHEKHER <100855002+klishtshashank@users.noreply.github.com>
2022-11-04 21:52:27 +0530

    Time: 12 ms (67.07%), Space: 7.6 MB (85.11%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -11,24 +11,26 @@ class Solution {
     
 public:
     string reverseVowels(string s) {
-         int k= s.size();
-        int i=0,j=k-1;
-        while(i<=j){
-            if(isVowel(s[i])&& isVowel(s[j]))
+         int n= s.size(); //size of word
+    int start =0, end=n-1; //two pointer one is at start and other at end
+        
+        while(start<=end){
+            if(isVowel(s[start])&& isVowel(s[end])) //if both are vowels
             {
-                swap(s[i++], s[j--]);
+                swap(s[start++], s[end--]); // swap them
             }
-            else if(isVowel(s[i]) && !isVowel(s[j]))
+            else if(isVowel(s[start]) && !isVowel(s[end])) //start is vowel, end is not
             {
-                j--;
+                end--;
             }
-            else if(!isVowel(s[i]) && isVowel(s[j]))
+            else if(!isVowel(s[start]) && isVowel(s[end])) //similar as above
             {
-                i++;
+                start++;
             }
-            else{
-                i++;
-                j--;
+            else{ // agar dono me se koi v vowel nhi hua to, 
+                //start aur end dono ko shift kr denge
+                start++;
+                end--;
             }
         }
         return s;
