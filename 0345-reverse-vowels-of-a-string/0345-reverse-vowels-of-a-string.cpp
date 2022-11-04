7bf7e39363f48aacc1f8b6354d60bf720f177128
SHASHANK SHEKHER <100855002+klishtshashank@users.noreply.github.com>
2022-11-04 22:00:30 +0530

    Time: 17 ms (45.43%), Space: 7.7 MB (61.66%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,30 +1,30 @@
 class Solution {
+ private:
     bool isVowel(char ch){
-        if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-            return true;
-            
+        return  (ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')||
+                (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u');
         }
-        else 
-            return false;
-    }
-    
     
 public:
     string reverseVowels(string s) {
          int n= s.size(); //size of word
-    int start =0, end=n-1; //two pointer one is at start and other at end
+    int start =0; //ek pointer start me 
+    int end=n-1; //aur ek end me le lete hain initially
         
         while(start<=end){
             if(isVowel(s[start])&& isVowel(s[end])) //if both are vowels
             {
                 swap(s[start++], s[end--]); // swap them
             }
-            else if(isVowel(s[start]) && !isVowel(s[end])) //start is vowel, end is not
-            {
+            else if(isVowel(s[start]) && !isVowel(s[end]))
+            {  
+                //start jisko point kar rha wo vowel hai aur end wala nhi
+                // tab end wala ko ek step peeche laate hain, qki hmko
+                //end wala side v ek vowel chahiye taaki swap kr sake SO
                 end--;
             }
-            else if(!isVowel(s[start]) && isVowel(s[end])) //similar as above
-            {
+            else if(!isVowel(s[start]) && isVowel(s[end])) 
+            { //similar as above bas ye start side wala case hai
                 start++;
             }
             else{ // agar dono me se koi v vowel nhi hua to, 
