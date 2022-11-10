e49e117deaaf0ff830bdc69f0db399bf9cf2202b
SHASHANK SHEKHER <100855002+klishtshashank@users.noreply.github.com>
2022-11-10 18:29:32 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (84.62%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -3,28 +3,38 @@ public:
     int minimumOperations(vector<int>& nums) {
         if(nums.size()==1)
             return nums[0]==0 ? 0:1;
-        
-        
      //   sort(nums.begin(), nums.end());
-        priority_queue<int, vector<int>, greater<int>>pq(nums.begin(), nums.end());
+//         priority_queue<int, vector<int>, greater<int>>pq(nums.begin(), nums.end());
+//         int ans=0;
+//         while(!pq.empty())
+//         {
+//             if(pq.top()==0) 
+//                 pq.pop();
+//             else{
+//                 int sz=pq.size();
+//                 vector<int>v;
+//                 int top= pq.top();
+                
+//                 for(int i=0;i<sz;i++)
+//                 {
+//                     v.push_back(pq.top()-top);
+//                     pq.pop();
+//                 }
+                
+//                 for(int i=0;i<v.size();i++) 
+//                     pq.push(v[i]);
+                
+//                 ans++;
+//             }
+//         }
+//         return ans;
         int ans=0;
-        while(!pq.empty())
+        unordered_map<int, int> m;
+        for(int i=0;i<nums.size();i++)
         {
-            if(pq.top()==0) pq.pop();
-            else{
-                int sz=pq.size();
-                vector<int>v;
-                int top= pq.top();
-                for(int i=0;i<sz;i++)
-                {
-                    v.push_back(pq.top()-top);
-                    pq.pop();
-                }
-                
-                for(int i=0;i<v.size();i++) pq.push(v[i]);
-                
-                ans++;
-            }
+           if(nums[i]!=0 && m[nums[i]]==0) ans++;
+            
+            m[nums[i]]++;
         }
         return ans;
     }
