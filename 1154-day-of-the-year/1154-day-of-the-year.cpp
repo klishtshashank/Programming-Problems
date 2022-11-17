833e726600502b3d84cc5b5b9dc34bdc1ac14baf
SHASHANK SHEKHER <100855002+klishtshashank@users.noreply.github.com>
2022-11-18 00:01:03 +0530

    Time: 59 ms (24.59%), Space: 5.8 MB (95.59%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.cpp
+++ 1154-day-of-the-year/1154-day-of-the-year.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     bool isLeapYear(int y){
-        if(y%400 == 0 || (y%4 == 0 && y%100 !=0))
+        if(y%400 == 0) 
             return true;
+        if((y%100 ==0))
+        return false;
+        if(y%4==0) return true;
+        
         return false;
     }
     int dayOfYear(string date) {
